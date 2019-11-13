38ea6d928b8414489a9dbd5ac1666b6b038add2e
Brian <brianpeguero@gmail.com>
2019-11-13 12:47:08 -0500

    creted the user and message classes

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 class User {
     constructor(name) {
         this.name = name
+        this.block = false
         this.inbox = []
     }
 }
 
 class Message {
     constructor(sender, content) {
-        
+        this.sender = sender
+        this.content = content
+        this.read = false
     }
 }
+
